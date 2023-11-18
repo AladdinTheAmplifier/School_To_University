7d3e4d6563cddd491777678259327868d93ffb4d
AladdinTheAmplifier <146553181+AladdinTheAmplifier@users.noreply.github.com>
2023-11-18 17:25:22 +0530

    Child Admission Registry Service and Common Lib Service

---
+++ s2u/s2u-child-admission-registry-service/src/main/java/com/s2u/childadmissionregistry/S2uChildAdmissionRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.s2u.childadmissionregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2uChildAdmissionRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2uChildAdmissionRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ s2u/s2u-common-lib/src/test/java/com/s2u/common/S2uCommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2u.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2uCommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
